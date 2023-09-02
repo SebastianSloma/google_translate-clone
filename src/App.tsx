8f0b4c00c0b58169d1d8ef418a002c616bcf1697
Sebastian <sebastiansloma@gmail.com>
2023-09-02 13:05:36 +0200

    button

--- src/App.tsx
+++ src/App.tsx
@@ -59,6 +59,13 @@ function App() {
 	return (
 		<div className='App'>
 			<h1>Google Translate *</h1>
+			<button
+				onClick={() => {
+					dispatch({ type: 'S' });
+				}}
+			>
+				Cambiar a Espanol
+			</button>
 		</div>
 	);
 }
