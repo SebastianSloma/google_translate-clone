05b56bc06b8488d804537c66e4d2125660d7f2a6
Sebastian <sebastiansloma@gmail.com>
2023-09-07 10:45:56 +0200

    added constans

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 import './App.css';
+import { useStore } from './hooks/useStore';
 
 function App() {
+	const { fromLanguage, setFromLanguage } = useStore();
 	return (
 		<div className='App'>
 			<h1>Google Translate *</h1>
 			<button
 				onClick={() => {
-					dispatch({ type: 'SET_FROM_LANGUAGE', payload: 'es' });
+					setFromLanguage('es');
 				}}
 			>
 				Cambiar a Espanol
